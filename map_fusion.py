80ad749fb7c67d9fdce477497eae31b9fd129a71
myeongw002 <117157396+myeongw002@users.noreply.github.com>
2025-07-23 16:16:04 +0900

    Update map_fusion.py

--- map_fusion.py
+++ map_fusion.py
@@ -5,32 +5,32 @@ import glob
 
 # 입력 경로 설정
 pose_npy_path = '/home/myungw00/kiss_slam/slam_output/2025-07-22_16-43-16/2024-06-15-11-44-28_0_poses.npy'
-ply_folder = '/home/myungw00/kiss_slam/slam_output/2025-07-22_16-43-16/pcd'
-output_path = '/home/myungw00/kiss_slam/slam_output/2025-07-22_16-43-16/merged_map.ply'
+pcd_folder = '/home/myungw00/kiss_slam/slam_output/2025-07-22_16-43-16/pcd'
+output_path = '/home/myungw00/kiss_slam/slam_output/2025-07-22_16-43-16/merged_map.pcd'
 
 # 1. pose 불러오기
 poses = np.load(pose_npy_path)  # shape: (n, 4, 4)
 
 # 2. ply 파일 정렬
-ply_files = sorted(glob.glob(os.path.join(ply_folder, '*.ply')))
+pcd_files = sorted(glob.glob(os.path.join(pcd_folder, '*.pcd')))
 
-print(f"Num Poses: {len(poses)}, Num PCDs: {len(ply_files)}")
+print(f"Num Poses: {len(poses)}, Num PCDs: {len(pcd_files)}")
 
-if len(poses) != len(ply_files):
-    print(f"경고: pose 수({len(poses)})와 ply 파일 수({len(ply_files)})가 다릅니다. 인덱스 매칭 확인 필요.")
+if len(poses) != len(pcd_files):
+    print(f"경고: pose 수({len(poses)})와 pcd 파일 수({len(pcd_files)})가 다릅니다. 인덱스 매칭 확인 필요.")
 
 all_points = []
 all_colors = []
 
 print()
-for idx, ply_file in enumerate(ply_files):
+for idx, pcd_file in enumerate(pcd_files):
     if idx >= len(poses):
-        print(f"ply 파일이 pose 개수보다 많음, 이후 파일은 무시됩니다.")
+        print(f"pcd 파일이 pose 개수보다 많음, 이후 파일은 무시됩니다.")
         break
     pose = poses[idx]  # shape (4, 4)
-    pcd = o3d.io.read_point_cloud(ply_file)
-    print(f"Pose: {pose} \n PCD: {ply_file}")
-    voxel_size = 0.1  # voxel 크기(단위=ply와 동일, 예: 5cm)로 조절
+    pcd = o3d.io.read_point_cloud(pcd_file)
+    print(f"Pose: {pose} \n PCD: {pcd_file}")
+    voxel_size = 0.1  # voxel 크기
     pcd = pcd.voxel_down_sample(voxel_size=voxel_size)
     # 3xN 점 행렬로 변환
     points = np.asarray(pcd.points)
@@ -53,7 +53,7 @@ else:
     print(f"원본 점 개수: {len(merged_pcd.points)}")
 
     # ---- 여기서 Voxel Downsampling 적용 ----
-    voxel_size = 0.1  # voxel 크기(단위=ply와 동일, 예: 5cm)로 조절
+    voxel_size = 0.1  # voxel 크기
     down_pcd = merged_pcd.voxel_down_sample(voxel_size=voxel_size)
     print(f"Voxel 적용 후 점 개수: {len(down_pcd.points)}")
 
